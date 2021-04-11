5e40fd31c5346519e8d267e6b7b44599c080ad57
Deshen <deshen.padayachee@sqn.world>
2021-04-11 14:47:41 +0200

    make sure header works on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import {
 
 const Header:FC = () => {
   return (
-    <header className='p-8 flex flex-row justify-between items-center max-w-7xl mx-auto mt-0 rounded-b-3xl shadow-lg'>
+    <header className='p-8 flex flex-row justify-between items-center flex-wrap max-w-7xl mx-auto mt-0 rounded-b-3xl shadow-lg'>
       <div className="flex items-center">
         <img src="/logo.png" />
         <h1 className="text-3xl ml-8">CheapShark</h1>
